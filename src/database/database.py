a2ad2fe8da5a230a461ad6a0cb14306ea9e3ed55
Ivan <ideshchenko@yandex.ru>
2024-11-14 16:13:03 +0300

    добавил метод __repr__

--- src/database/database.py
+++ src/database/database.py
@@ -7,4 +7,13 @@ session_factory = sessionmaker(engine)
 
 
 class Base(DeclarativeBase):
-    pass
+
+    repr_cols_num = 3
+    repr_cols = tuple()
+    
+    def __repr__(self) -> str:
+        cols = []
+        for i, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or i < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
